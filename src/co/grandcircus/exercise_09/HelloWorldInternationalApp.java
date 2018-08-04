94f1fdc7ab8ddddb176fa94303f86ef2923d5838
louis-m-privette-iii <louis.privette@gmail.com>
2018-08-04 13:46:08 -0400

    Initial Comitt. Exercise Incomplete.

---
+++ src/co/grandcircus/exercise_09/HelloWorldInternationalApp.java
@@ -0,0 +1,13 @@
+package co.grandcircus.exercise_09;
+
+// Task: Exercise 09 - Prompt the user to enter a language. Based on the language the user input, display "Hello, World!"
+// in that language. Use a switch statement to choose what to display.
+
+public class HelloWorldInternationalApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
